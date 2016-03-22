d70d1b5feb72c7b81592bff1cbbfe500c1510040
zverok <zverok.offline@gmail.com>
2016-03-22 22:08:21 +0200

    Add World#bubbles

--- examples/bubbles.rb
+++ examples/bubbles.rb
@@ -6,29 +6,26 @@ require 'ostruct'
 include Math
 using Worldize::Refinements
 
-countries = File.read('data/countries.geojson').
+countries =
+  File.read('data/countries.geojson').
         derp{|json| JSON.parse(json)}.
         derp{|hash| Hashie::Mash.new(hash)}.
         features
 
+capitals =
+  CSV.read(File.expand_path('../data/population_by_capital.csv', __FILE__), headers: true, converters: [:integer, :float]).
+      map{|row| OpenStruct.new(row.to_h)}.
+      select(&:population) # dropping capitals with population unknown
+
 map = Worldize::World.new(width: 2000, ocean: 'white')
-map.countries
 
-capitals = CSV.read(File.expand_path('../data/population_by_capital.csv', __FILE__), headers: true, converters: [:integer, :float]).
-            map{|row| OpenStruct.new(row.to_h)}.
-            select(&:population) # dropping capitals with population unknown
-
-max_pop = capitals.map(&:population).max
-max_r = 50
-max_area = PI*max_r**2
-
-capitals.sort_by(&:population).reverse.each do |row|
-  area = row.population.rescale(0..max_pop, 0..max_area)
-  r = sqrt(area / PI)
-  map.circle(row.lat, row.lng,
-    radius: r, color: 'blue', fill: 'blue',
-    transparency: row.population.rescale(0..max_pop, 0..0.8))
-end
+map.
+  countries.
+  bubbles(
+    capitals.map{|r| [[r.lat, r.lng], r.population]}.to_h,
+    color: 'blue',
+    max_radius: 60
+  )
 
 capitals.sort_by(&:population).reverse.each do |row|
   map.text(row.lat, row.lng, row.name, to: :north_east, size: 10, style: :normal)
--- lib/worldize/world.rb
+++ lib/worldize/world.rb
@@ -32,10 +32,57 @@ module Worldize
     def countries(value_by_country = nil, **options)
       # NB: syntax countries(value_by_country = {}, **options) causes segfault in Ruby 2.2.0
       value_by_country ||= {}
+
       options = DEFAULT_COLORS.merge(options)
+
       border = options.fetch(:border)
       palette = Palette.make(options.fetch(:palette))
+      countries = prepare_countries(value_by_country, palette)
+
+      @countries.each do |country|
+        bg = countries[country.properties.name] ||
+              countries[country.properties.iso_a3] ||
+              options.fetch(:land)
+              
+        feature(country.geometry, color: border, fill: bg)
+      end
+
+      self
+    end
+
+    def bubbles(value_by_place = nil, **options)
+      value_by_country ||= {}
+
+      options = DEFAULT_COLORS.merge(options)
       
+      max = value_by_place.values.max
+      max_r = options.fetch(:max_radius, 50)
+      max_area = PI*max_r**2
+
+      color = options.fetch(:color, 'blue')
+
+      value_by_place.sort_by(&:last).reverse.each do |(lat, lng), value|
+        area = value.rescale(0..max, 0..max_area)
+        r = sqrt(area / PI)
+        circle(lat, lng,
+          radius: r, color: color,
+          transparency: value.rescale(0..max, 0..0.8))
+      end
+
+      self
+    end
+
+    def render
+      # really meaningful lat: -63..83, everything else is, in fact, poles
+      ymin = lat2y(84, width)
+      ymax = lat2y(-63, width)
+      
+      super.crop(0, ymin, width, ymax-ymin, true)
+    end
+
+    private
+
+    def prepare_countries(value_by_country, palette)
       colored = value_by_country.select{|_, value| value.is_a?(String)}
       measured = value_by_country.select{|_, value| value.is_a?(Numeric)}
       highlighted = value_by_country.select{|_, value| value.is_a?(TrueClass)}
@@ -49,28 +96,7 @@ module Worldize
         map{|country, value| [country, palette.pct(value.rescale(min..max, 0..100))]}.
         to_h
 
-      countries = colored.
-        merge(highlighted).
-        merge(measured)
-
-
-      @countries.each do |country|
-        bg = countries[country.properties.name] ||
-              countries[country.properties.iso_a3] ||
-              options.fetch(:land)
-              
-        feature(country.geometry, color: border, fill: bg)
-      end
-
-      self
-    end
-
-    def render
-      # really meaningful lat: -63..83, everything else is, in fact, poles
-      ymin = lat2y(84, width)
-      ymax = lat2y(-63, width)
-      
-      super.crop(0, ymin, width, ymax-ymin, true)
+      colored.merge(highlighted).merge(measured)
     end
   end
 end
